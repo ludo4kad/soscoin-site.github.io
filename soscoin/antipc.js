230ae7356c8cb81cc81431b1a108f8e7b5ce445c
soscoin-site <testingggogle@gmail.com>
2024-08-13 01:36:03 +0300

    Add files via upload

--- soscoin/antipc.js
+++ soscoin/antipc.js
@@ -1,1 +1,5 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {} else {window.location.href="/";}
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {} else 
+{ if (navigator.userAgent.startsWith('Mozilla/5.0 (Linux; Android 6.0;')){
+window.location.href="/";
+}
+}
