94c03c08783b77417c395a00966d1e570727b59a
soscoin-site <testingggogle@gmail.com>
2024-08-15 13:00:15 +0300

    Add files via upload

--- soscoin/notification.js
+++ soscoin/notification.js
@@ -2,7 +2,7 @@ function showNotification(textTo){
   var notifDiv = document.getElementById('notif');
   notifDiv.style.display = "block";
   notifDiv.innerHTML = textTo + "<br><button onclick='hideNotification()'>Закрыть</button>";
-}
+}//ss
 function hideNotification() {
   var notifDiv = document.getElementById('notif');
   notifDiv.style.display = "none";
--- soscoin/script-soscoin.js
+++ soscoin/script-soscoin.js
@@ -1,4 +1,19 @@
 let allSos = localStorage.getItem('allSos') || 0;
+let level = localStorage.getItem('allSos') || 1;
+
+function newLevel() {
+  if (allSos >= 100*level){//ss
+  level++;
+  localStorage.setItem('userLevel', level);
+  allSos -= level * 100;
+  localStorage.setItem('allSos', allSos);
+} else {showNotification('Недостаточно средств');}
+}
+
+function unLevel(val){
+  level -= val;
+  localStorage.setItem('userLevel', level);
+}
 
 function incrementClicks() {
   const box = document.querySelector('img.tap');
@@ -11,6 +26,8 @@ function incrementClicks() {
 }
 
 function updateTxt(){
+  document.getElementById("reactor").text = "Прокачать уровень ("+ level*100 +")";
+  document.getElementById("level").innerHTML = level;
   document.getElementById("Y2xpY").innerHTML = "<img src='images/coin.png' style='width:4vh' alt='coin'>"+allSos;
 }
 var timer = setInterval(updateTxt, 1);
